79e8efa37522b1e7cab86ab0c709f68d0323515c
Task2401 <ana.chr.mr@gmail.com>
2026-06-29 15:50:19 +0300

    update is 45% ready

--- src/State.cpp
+++ src/State.cpp
@@ -15,6 +15,16 @@ static float randf() {
     return (float)rand() / (float)RAND_MAX;
 }
 
+static Position getRandomPosition() {
+    return Position{ randi(0, 29), randi(0,29) };
+}
+
+static int  calculateDistance(Position p1, Position p2) {
+    int dx = abs(p1.x - p2.x);
+    int dy = abs(p1.y - p2.y);
+    return dx + dy;
+}
+
 State::State() : snake(RIGHT), score(0), frameCounter(0), gameOver(false), playing(false), paused(false), keys{false, false, false, false, false, false} {
     cout << "State created succesfully!" << endl;
 }
@@ -52,5 +62,82 @@ void State::addObject(const GameObject& obj) {
 }
 
 void State::update() {
+    Position headPos = snake.getHeadPos();
+    Direction headDir = snake.getSnakeDiretion();
+    frameCounter++;
 
+    if (playing == false) {
+        if (keys.start == true) {
+            playing = true;
+            keys.start = false;
+        }
+        return;
+    }
+
+    if (gameOver) {
+        if (keys.start) {
+            score = 0;
+            frameCounter = 0;
+            gameOver = false;
+            paused = false;
+            playing = true;
+            snake = Snake(RIGHT);
+            objects.clear();
+            keys.start = false;
+        }
+        return;
+    }
+
+    if (keys.pause) {
+        paused = !paused;
+        keys.pause = false;
+    }
+
+    if (paused) return;
+
+
+    if (frameCounter % FRAMES_PER_STATE == 0) {
+        
+        if (keys.up && headDir != DOWN)
+            snake.setDirection(UP);
+
+        else if (keys.down && headDir != UP)
+            snake.setDirection(DOWN);
+
+        else if (keys.left && headDir != RIGHT)
+            snake.setDirection(LEFT);
+
+        else if (keys.right && headDir != LEFT)
+            snake.setDirection(RIGHT);
+
+        snake.move();
+
+        int appleCount = 0;
+        int eagleCount = 0;
+
+        for (size_t i = 0; i < objects.size(); i++) {
+            if (objects[i].getType() == APPLE) appleCount++;
+            else if (objects[i].getType() == EAGLE) eagleCount++;
+        }
+
+        while (eagleCount < MAX_EAGLE_COUNT) {
+            Position randomPos = getRandomPosition();
+            GameObject eagle = GameObject(EAGLE, randomPos, (Direction)randi(0, 3));
+            addObject(eagle);
+        }
+
+        while (appleCount < MAX_APPLE_COUNT) {
+            Position randomPos = getRandomPosition();
+            GameObject apple = GameObject(APPLE, randomPos, UP);
+            addObject(apple);
+        }
+
+        for (size_t i = 0; i < objects.size(); i++) {
+            if (objects[i].getType() == EAGLE) {
+                if(randf() < EAGLE_NEXT_DIRECTION_PROBABILITY) 
+                    objects[i].setDirection((Direction)randi(0, 3));
+                objects[i].move();
+            }
+        }
+    }
 }
